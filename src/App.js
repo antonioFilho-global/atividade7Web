9e2425061f4f804e9aca2c4e5c469f5aa3602993
antonio alves costa filho <antonio.alves@estagiario.ufca.edu.br>
2021-12-08 21:01:37 -0300

    Atividade 7

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import MeuComponente from './MeuComponente';
+
+function App() {
+  return (
+    <div className="App">
+      App Atividade
+    
+      <MeuComponente/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MeuComponente.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MeuComponente() {
+    return (
+        <h1>Meu Componente</h1>
+    )
+}
+
+export default MeuComponente
